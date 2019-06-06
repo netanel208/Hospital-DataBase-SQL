32ba5c2a96e3637631e60b8b37de2c32f47a63c0
Netanel Davidov <44946807+netanel208@users.noreply.github.com>
2019-06-06 23:39:33 +0300

    Update quastions.sql

--- quastions.sql
+++ quastions.sql
@@ -1,13 +1,13 @@
 use hospital;
 
 /*Q1*/
-/*SELECT patients.patient_id , patient_name, appointment_time FROM appointment 
+SELECT patients.patient_id , patient_name, appointment_time FROM appointment 
 inner join patients on appointment.patient_id = patients.patient_id
 where appointment.doctor_id = 111111111
-order by appointment_time;*/
+order by appointment_time;
 
 /*Q2*/
-/*
+
 drop procedure enter_patient;
 delimiter $$
 create procedure enter_patient (in patientId int, in doctorId int)
@@ -16,19 +16,19 @@ insert into hospital.queue value(
 (select appointment_id from hospital.appointment where patient_id = patientId and doctor_id = doctorId),
 (select now()));
 end $$
-delimiter ;*/
+delimiter ;
 
 
 /*Q3*/
-/*create view long_time_waiting_view as
+create view long_time_waiting_view as
 select rel.patient_id from
 (select appointment.patient_id, appointment.appointment_time, queue.actual_time, (queue.actual_time - appointment.appointment_time)as diff 
 from hospital.appointment
 inner join hospital.queue on appointment.appointment_id = queue.appointment_id
-order by diff desc) as rel LIMIT 10;*/
+order by diff desc) as rel LIMIT 10;
 
 /*Q4*/
-/*DELIMITER $$
+DELIMITER $$
 CREATE TRIGGER after_delete_trigger 
 after delete on hospital.queue
  for each row 
@@ -61,4 +61,4 @@ update hospital.queue_summary inner join
  set queue_summary.num_of_patients = t.num_of_patients;
 
  end $$
- DELIMITER ;*/
+ DELIMITER ;
